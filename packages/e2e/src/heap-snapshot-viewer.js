aff4573b2a6e14eababea74e234c9436bdc221cb
Le Vivilet <levivlet@pm.me>
2024-09-07 16:33:36 +0200

    rename

--- packages/e2e/src/heap-snapshot-viewer.js
+++ packages/e2e/src/heap-snapshot-viewer.js
@@ -3,5 +3,7 @@ export const name = 'media-preview'
 export const skip = true
 
 export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
-  // TODO verify heapsnapshot can be opened
+  // TODO verify media preview opens
+  // TODO add tests for different files types
+  // TODO add tests for errors
 }
