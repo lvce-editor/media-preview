8081764685e98873168fbaa5d2ee21c8f81b4e5a
Le Vivilet <levivlet@pm.me>
2024-09-07 19:14:03 +0200

    fix url

--- packages/extension/src/parts/HeapSnapshotWorkerUrl/HeapSnapshotWorkerUrl.ts
+++ packages/extension/src/parts/HeapSnapshotWorkerUrl/HeapSnapshotWorkerUrl.ts
@@ -1,3 +1,3 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
 
-export const heapSnapshotWorkerUrl = `${AssetDir.assetDir}/heap-snapshot-worker/src/heapSnapshotWorkerMain.ts`
+export const mediaPreviewWorkerUrl = `${AssetDir.assetDir}/media-preview-worker/src/mediaPreviewWorkerMain.ts`
