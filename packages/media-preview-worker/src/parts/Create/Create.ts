20ee765854fc7b47317272b6c5969f369570e003
Le Vivilet <levivlet@pm.me>
2024-09-08 09:47:07 +0200

    create dom matrix

--- packages/media-preview-worker/src/parts/Create/Create.ts
+++ packages/media-preview-worker/src/parts/Create/Create.ts
@@ -1,9 +1,11 @@
 import * as PreviewState from '../PreviewState/PreviewState.ts'
+import * as DomMatrix from '../DomMatrix/DomMatrix.ts'
 
 export const create = (id: number) => {
   const preview = {
     x: 0,
     y: 0,
+    domMatrix: DomMatrix.create(),
   }
   PreviewState.set(id, preview)
 }
