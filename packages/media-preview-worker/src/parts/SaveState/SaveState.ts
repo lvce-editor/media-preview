a0338517a57b331ce3aa786d86fa36bee721036f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-25 22:49:25 +0200

    feature: restore saved state (#30)

--- packages/media-preview-worker/src/parts/SaveState/SaveState.ts
+++ packages/media-preview-worker/src/parts/SaveState/SaveState.ts
@@ -1,8 +1,9 @@
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+import * as StringifyDomMatrix from '../StringifyDomMatrix/StringifyDomMatrix.ts'
 
 export const saveState = (id: number): any => {
   const webView = WebViewStates.get(id)
   return {
-    domMatrix: `${webView.domMatrix}`,
+    domMatrix: StringifyDomMatrix.stringifyDomMatrix(webView.domMatrix),
   }
 }
