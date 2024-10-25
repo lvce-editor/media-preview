ded744d36360d30d0f4478760e03163e0f4e3d2d
Le Vivilet <levivlet@pm.me>
2024-10-25 22:47:49 +0200

    feature: restore saved state

--- packages/media-preview-worker/src/parts/SetSavedState/SetSavedState.ts
+++ packages/media-preview-worker/src/parts/SetSavedState/SetSavedState.ts
@@ -1,3 +1,22 @@
+import * as DomMatrix from '../DomMatrix/DomMatrix.ts'
+import type { WebView } from '../WebView/WebView.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+
+const getSavedDomMatrix = (savedState: any): DOMMatrixReadOnly => {
+  if (savedState && savedState.domMatrix && typeof savedState.domMatrix === 'string') {
+    const maybe = new DOMMatrixReadOnly(savedState.domMatrix)
+    // TODO how to verify that dom matrix is valid?
+    return maybe
+  }
+  return DomMatrix.create()
+}
+
 export const setSavedState = (id: number, savedState: any): void => {
-  // TODO
+  const webView = WebViewStates.get(id)
+  const domMatrix = getSavedDomMatrix(savedState)
+  const newWebView: WebView = {
+    ...webView,
+    domMatrix,
+  }
+  WebViewStates.set(id, newWebView)
 }
--- packages/media-preview-worker/src/parts/StringifyDomMatrix/StringifyDomMatrix.ts
+++ packages/media-preview-worker/src/parts/StringifyDomMatrix/StringifyDomMatrix.ts
@@ -1,5 +1,5 @@
 // workaround for browser bug
-export const stringifyDomMatrix = (domMatrix: DOMMatrix): string => {
+export const stringifyDomMatrix = (domMatrix: DOMMatrix | DOMMatrixReadOnly): string => {
   const { a, b, c, d, e, f } = domMatrix
   return `matrix(${a}, ${b}, ${c}, ${d}, ${e}, ${f})`
 }
