836e30ef91add5dad283492f148aa5a5618afbfc
Le Vivilet <levivlet@pm.me>
2024-10-25 22:50:45 +0200

    feature: rename preview to webview

--- packages/media-preview-worker/src/parts/HandlePointerDown/HandlePointerDown.ts
+++ packages/media-preview-worker/src/parts/HandlePointerDown/HandlePointerDown.ts
@@ -1,14 +1,14 @@
 import type { WebView } from '../WebView/WebView.ts'
-import * as PreviewStates from '../WebViewStates/WebViewStates.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handlePointerDown = (id: number, x: number, y: number): WebView => {
-  const state = PreviewStates.get(id)
+  const state = WebViewStates.get(id)
   const newState: WebView = {
     ...state,
     pointerOffsetX: x,
     pointerOffsetY: y,
     pointerDown: true,
   }
-  PreviewStates.set(id, newState)
+  WebViewStates.set(id, newState)
   return newState
 }
--- packages/media-preview-worker/src/parts/HandlePointerUp/HandlePointerUp.ts
+++ packages/media-preview-worker/src/parts/HandlePointerUp/HandlePointerUp.ts
@@ -1,12 +1,12 @@
 import type { WebView } from '../WebView/WebView.ts'
-import * as PreviewStates from '../WebViewStates/WebViewStates.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handlePointerUp = (id: number, x: number, y: number): WebView => {
-  const state = PreviewStates.get(id)
+  const state = WebViewStates.get(id)
   const newState: WebView = {
     ...state,
     pointerDown: false,
   }
-  PreviewStates.set(id, newState)
+  WebViewStates.set(id, newState)
   return newState
 }
