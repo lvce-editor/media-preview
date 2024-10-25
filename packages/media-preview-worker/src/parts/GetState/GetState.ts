c0f37336eea264c2e9c01297f445a094022d288f
Le Vivilet <levivlet@pm.me>
2024-10-25 22:52:32 +0200

    feature: add getState function

---
+++ packages/media-preview-worker/src/parts/GetState/GetState.ts
@@ -0,0 +1,7 @@
+import { WebView } from '../WebView/WebView.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+
+export const getState = (id: number): WebView => {
+  const webView = WebViewStates.get(id)
+  return webView
+}
