653ef2417dfe575111ed399b12f9ce8cff3cb835
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-25 22:44:17 +0200

    feature: add explicit return types for dom matrix (#29)

--- packages/media-preview-worker/src/parts/DomMatrix/DomMatrix.ts
+++ packages/media-preview-worker/src/parts/DomMatrix/DomMatrix.ts
@@ -1,8 +1,8 @@
-export const create = (init = [1, 0, 0, 1, 0, 0]) => {
+export const create = (init = [1, 0, 0, 1, 0, 0]): DOMMatrixReadOnly => {
   return new DOMMatrixReadOnly(init)
 }
 
-export const scaleUp = (domMatrix: DOMMatrixReadOnly, deltaScale: number) => {
+export const scaleUp = (domMatrix: DOMMatrixReadOnly, deltaScale: number): DOMMatrixReadOnly => {
   return new DOMMatrixReadOnly([
     domMatrix.a * deltaScale,
     domMatrix.b,
@@ -13,11 +13,11 @@ export const scaleUp = (domMatrix: DOMMatrixReadOnly, deltaScale: number) => {
   ])
 }
 
-export const scaleDown = (domMatrix: DOMMatrixReadOnly, deltaScale: number) => {
+export const scaleDown = (domMatrix: DOMMatrixReadOnly, deltaScale: number): DOMMatrixReadOnly => {
   return scaleUp(domMatrix, 1 / deltaScale)
 }
 
-export const zoomInto = (domMatrix: DOMMatrixReadOnly, zoomFactor: number, relativeX: number, relativeY: number) => {
+export const zoomInto = (domMatrix: DOMMatrixReadOnly, zoomFactor: number, relativeX: number, relativeY: number): DOMMatrix => {
   return new DOMMatrix()
     .translateSelf(relativeX, relativeY)
     .scaleSelf(zoomFactor)
@@ -26,11 +26,11 @@ export const zoomInto = (domMatrix: DOMMatrixReadOnly, zoomFactor: number, relat
 }
 
 // workaround for browser bug
-export const toString = (domMatrix: DOMMatrixReadOnly) => {
+export const toString = (domMatrix: DOMMatrixReadOnly): string => {
   const { a, b, c, d, e, f } = domMatrix
   return `matrix(${a}, ${b}, ${c}, ${d}, ${e}, ${f})`
 }
 
-export const move = (domMatrix: DOMMatrixReadOnly, deltaX: number, deltaY: number) => {
+export const move = (domMatrix: DOMMatrixReadOnly, deltaX: number, deltaY: number): DOMMatrixReadOnly => {
   return new DOMMatrixReadOnly([domMatrix.a, domMatrix.b, domMatrix.c, domMatrix.d, domMatrix.e + deltaX, domMatrix.f + deltaY])
 }
