633dd035531b7306bc7083c0dcd5eaca3c8f5d8a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-25 22:51:58 +0200

    feature: rename preview to webview (#31)

--- packages/media-preview-worker/src/parts/HandlePointerMove/HandlePointerMove.ts
+++ packages/media-preview-worker/src/parts/HandlePointerMove/HandlePointerMove.ts
@@ -1,9 +1,9 @@
 import * as DomMatrix from '../DomMatrix/DomMatrix.ts'
 import type { WebView } from '../WebView/WebView.ts'
-import * as PreviewStates from '../WebViewStates/WebViewStates.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handlePointerMove = (id: number, x: number, y: number): WebView => {
-  const state = PreviewStates.get(id)
+  const state = WebViewStates.get(id)
   const { pointerOffsetX, pointerOffsetY, domMatrix } = state
   const deltaX = x - pointerOffsetX
   const deltaY = y - pointerOffsetY
@@ -14,6 +14,6 @@ export const handlePointerMove = (id: number, x: number, y: number): WebView => 
     pointerOffsetY: y,
     domMatrix: newDomMatrix,
   }
-  PreviewStates.set(id, newState)
+  WebViewStates.set(id, newState)
   return newState
 }
