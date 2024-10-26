6ed6fdc353f3a3658f082b7ae524886e74dda928
Le Vivilet <levivlet@pm.me>
2024-10-26 12:38:22 +0200

    feature: pass webview id to remoteUrl function

--- packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
+++ packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
@@ -6,7 +6,10 @@ export const webViewProvider = {
   id: 'builtin.media-preview',
   async create(webView, uri, savedState) {
     // TODO if can use remote uri, use remote uri, else read file
-    const remoteUrl = await MediaPreview.getUrl(uri)
+    // @ts-ignore
+    const remoteUrl = await vscode.getRemoteUrl(uri, {
+      webViewId: webViewProvider.id,
+    })
     await MediaPreview.create(id)
     await MediaPreview.setSavedState(id, savedState)
     await webView.invoke('initialize', remoteUrl)
