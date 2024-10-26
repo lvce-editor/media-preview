a5e75f6e0527c7f7031b172195e886f941b1a76c
Le Vivilet <levivlet@pm.me>
2024-10-26 08:27:33 +0200

    feature: add minZoom and maxZoom to state

--- packages/media-preview-worker/src/parts/WebView/WebView.ts
+++ packages/media-preview-worker/src/parts/WebView/WebView.ts
@@ -3,4 +3,8 @@ export interface WebView {
   readonly pointerOffsetY: number
   readonly domMatrix: DOMMatrixReadOnly
   readonly pointerDown: boolean
+  readonly minZoom: number
+  readonly maxZoom: number
+  readonly zoomFactor: 200
+  readonly eventCache: readonly any[]
 }
