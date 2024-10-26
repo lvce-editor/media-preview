089aa6e6fde97d178657c5b62c307731780cc042
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-26 08:27:18 +0200

    feature: move media preview ipc into separate file (#40)

---
+++ packages/extension/src/parts/MediaPreview/MediaPreview.ts
@@ -0,0 +1,33 @@
+import * as MediaPreviewWorker from '../MediaPreviewWorker/MediaPreviewWorker.ts'
+
+export const getUrl = (uri: string) => {
+  return MediaPreviewWorker.invoke('MediaPreview.getUrl', uri)
+}
+
+export const create = (id: number) => {
+  return MediaPreviewWorker.invoke('MediaPreview.create', id)
+}
+
+export const setSavedState = (id: number, savedState: any) => {
+  return MediaPreviewWorker.invoke('MediaPreview.setSavedState', id, savedState)
+}
+
+export const getState = (id: number) => {
+  return MediaPreviewWorker.invoke('MediaPreview.getState', id)
+}
+
+export const saveState = (id: number) => {
+  return MediaPreviewWorker.invoke('MediaPreview.saveState', id)
+}
+
+export const handlePointerDown = (id: number, x: number, y: number) => {
+  return MediaPreviewWorker.invoke('MediaPreview.handlePointerDown', id, x, y)
+}
+
+export const handlePointerMove = (id: number, x: number, y: number) => {
+  return MediaPreviewWorker.invoke('MediaPreview.handlePointerMove', id, x, y)
+}
+
+export const handlePointerUp = (id: number, x: number, y: number) => {
+  return MediaPreviewWorker.invoke('MediaPreview.handlePointerUp', id, x, y)
+}
