fcd2ebf075f6ffbfb7942cff4915ff94d40d83c8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-01 12:55:42 +0100

    feature: add getRemoteUrl test (#55)

--- packages/media-preview-worker/src/parts/GetRemoteUrl/GetRemoteUrl.ts
+++ packages/media-preview-worker/src/parts/GetRemoteUrl/GetRemoteUrl.ts
@@ -1,4 +1,4 @@
-export const getRemoteUrl = (uri: string) => {
+export const getRemoteUrl = (uri: string): string => {
   // TODO maybe instead of hardcoding path,
   // ask extension api to generate a remote url for a file path
   // e.g. vscode.getRemoteUrl(uri)
