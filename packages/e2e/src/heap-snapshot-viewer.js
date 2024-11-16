85859bf5587bc7f34e5cf56603f1ae6938666838
Le Vivilet <levivlet@pm.me>
2024-11-16 23:16:49 +0100

    feature: add e2e test

--- packages/e2e/src/heap-snapshot-viewer.js
+++
@@ -1,9 +0,0 @@
-export const name = 'media-preview'
-
-export const skip = true
-
-export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
-  // TODO verify media preview opens
-  // TODO add tests for different files types
-  // TODO add tests for errors
-}
---
+++ packages/e2e/src/media-preview.js
@@ -0,0 +1,20 @@
+export const name = 'media-preview'
+
+export const test = async ({ Workspace, FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.svg`,
+    `<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="50" cy="50" r="50" fill="red" />
+</svg>`,
+  )
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.svg`)
+
+  // assert
+
+  // TODO verify that svg is visible
+}
