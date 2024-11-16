01e3638c91d48e34e7da6e6fa9248110ea1353f3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-16 23:24:03 +0100

    feature: add png e2e test (#65)

---
+++ packages/e2e/src/media-preview-png.js
@@ -0,0 +1,20 @@
+export const name = 'media-preview'
+
+export const test = async ({ Workspace, FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+
+  const url = new URL('../files/file.png', import.meta.url)
+  const response = await fetch(url)
+  const text = await response.text()
+  // TODO allow file system write to accept arraybuffer
+  await FileSystem.writeFile(`${tmpDir}/test.png`, text)
+  await Workspace.setPath(tmpDir)
+
+  // act
+  await Main.openUri(`${tmpDir}/test.png`)
+
+  // assert
+
+  // TODO verify that svg is visible
+}
