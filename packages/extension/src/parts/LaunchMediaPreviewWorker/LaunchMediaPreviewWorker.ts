c499f4ebfbbd6ff05c0549fb190c1029eb9a64c8
Le Vivilet <levivlet@pm.me>
2024-12-29 13:51:22 +0100

    feature: replace execute with commandmap

--- packages/extension/src/parts/LaunchMediaPreviewWorker/LaunchMediaPreviewWorker.ts
+++ packages/extension/src/parts/LaunchMediaPreviewWorker/LaunchMediaPreviewWorker.ts
@@ -1,15 +1,11 @@
 import * as MediaPreviewWorkerUrl from '../MediaPreviewWorkerUrl/MediaPreviewWorkerUrl.ts'
 
-const execute = (method, ...params) => {
-  return {}
-}
-
 export const launchMediaPreviewWorker = async () => {
   // @ts-ignore
   const rpc = await vscode.createRpc({
     url: MediaPreviewWorkerUrl.mediaPreviewWorkerUrl,
     name: 'Media Preview Worker',
-    execute,
+    commandMap: {},
   })
   return rpc
 }
