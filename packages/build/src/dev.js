d5ccca0978eb474e57116e686e0661acaf3feb62
Le Vivilet <levivlet@pm.me>
2025-06-27 17:29:46 +0200

    feature: update dev script

--- packages/build/src/dev.js
+++ packages/build/src/dev.js
@@ -1,15 +1,54 @@
-import { execa } from 'execa'
-import { root } from './root.js'
+import { spawn } from 'child_process'
+import { join } from 'path'
 
-const main = async () => {
-  execa(`npm`, ['run', 'build:watch'], {
-    cwd: root,
+const __dirname = import.meta.dirname
+
+const root = join(__dirname, '..', '..', '..')
+
+const serverPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'server', 'bin', 'server.js')
+const esbuildPath = join(root, 'packages', 'build', 'node_modules', '.bin', 'esbuild')
+
+const main = () => {
+  const child = spawn(serverPath, ['--only-extension=packages/extension', '--test-path=packages/e2e'], {
     stdio: 'inherit',
   })
-  execa('node', ['packages/server/node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
-    cwd: root,
-    stdio: 'inherit',
-  })
+  const child2 = spawn(
+    esbuildPath,
+    [
+      '--format=esm',
+      '--bundle',
+      '--external:node:buffer',
+      '--external:electron',
+      '--external:ws',
+      '--external:node:worker_threads',
+      '--bundle',
+      '--watch',
+      'packages/media-preview-worker/src/mediaPreviewWorkerMain.ts',
+      '--outfile=packages/media-preview-worker/dist/mediaPreviewWorkerMain.js',
+    ],
+    {
+      cwd: root,
+      stdio: 'inherit',
+    },
+  )
+  const child3 = spawn(
+    esbuildPath,
+    [
+      '--format=esm',
+      '--bundle',
+      '--external:node:buffer',
+      '--external:electron',
+      '--external:ws',
+      '--external:node:worker_threads',
+      '--bundle',
+      '--watch',
+      'packages/extension/src/mediaPreviewMain.ts',
+      '--outfile=packages/extension/dist/mediaPreviewMain.js',
+    ],
+    {
+      cwd: root,
+    },
+  )
 }
 
 main()
