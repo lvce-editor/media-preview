21941ad5903b5fd388a3869cd4ca39c4f0b7b759
Le Vivilet <levivlet@pm.me>
2025-06-29 22:05:13 +0200

    feature: move dommatrix into separate file

--- packages/media-preview-worker/src/parts/ParseDomMatrix/ParseDomMatrix.ts
+++ packages/media-preview-worker/src/parts/ParseDomMatrix/ParseDomMatrix.ts
@@ -1,30 +1,13 @@
 import * as DomMatrix from '../DomMatrix/DomMatrix.ts'
+import { parseDomMatrixInternal } from '../ParseDomMatrixInternal/ParseDomMatrixInternal.ts'
 
 const prefix = 'matrix('
 const postfix = ')'
-
-const parseDomMatrixInner = (innerString: string): DOMMatrixReadOnly => {
-  const parts = innerString.split(', ')
-  if (parts.length !== 6) {
-    return DomMatrix.create()
-  }
-  const [aString, bString, cString, dString, eString, fString] = parts
-  const a = parseFloat(aString)
-  const b = parseFloat(bString)
-  const c = parseFloat(cString)
-  const d = parseFloat(dString)
-  const e = parseFloat(eString)
-  const f = parseFloat(fString)
-  if (isNaN(a) || isNaN(b) || isNaN(c) || isNaN(d) || isNaN(e) || isNaN(f)) {
-    return DomMatrix.create()
-  }
-  return new DOMMatrixReadOnly([a, b, c, d, e, f])
-}
 
 export const parseDomMatrix = (domMatrixString: string): DOMMatrixReadOnly => {
   if (!domMatrixString || !domMatrixString.startsWith(prefix) || !domMatrixString.endsWith(postfix)) {
     return DomMatrix.create()
   }
   const inner = domMatrixString.slice(prefix.length, -postfix.length)
-  return parseDomMatrixInner(inner)
+  return parseDomMatrixInternal(inner)
 }
