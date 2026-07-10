7fe68e4e38518c9e2ba2168f595211d2c0440d38
Le Vivilet <levivilet@pm.me>
2026-07-10 21:33:01 +0000

    fix: silence ts6 only-throw lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,7 @@ export default [
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      '@typescript-eslint/only-throw-error': 'off',
     },
   },
 ]
