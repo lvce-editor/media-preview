364d90bfe09824e24e0b25a649ce4cfe1d80d8e4
Le Vivilet <levivilet@pm.me>
2026-07-31 19:32:18 +0000

    fix: follow virtual dom lint rules

--- packages/extension/src/parts/RenderMediaPreview/RenderMediaPreview.ts
+++ packages/extension/src/parts/RenderMediaPreview/RenderMediaPreview.ts
@@ -1,41 +1,49 @@
 import { text, VirtualDomElements, type VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { MediaPreviewState } from '../MediaPreviewViewInstance/MediaPreviewViewInstance.ts'
 
+const handleMediaPreviewImageError = 'handleMediaPreviewImageError'
+const handleMediaPreviewPointerDown = 'handleMediaPreviewPointerDown'
+const handleMediaPreviewWheel = 'handleMediaPreviewWheel'
+
+const errorNode: VirtualDomNode = {
+  childCount: 1,
+  className: 'MediaPreviewError',
+  type: VirtualDomElements.Div,
+}
+
+const errorMessageNode: VirtualDomNode = {
+  childCount: 1,
+  type: VirtualDomElements.Span,
+}
+
+const contentNode: VirtualDomNode = {
+  childCount: 1,
+  className: 'MediaPreviewContent',
+  type: VirtualDomElements.Div,
+}
+
+const imageWrapperNode: VirtualDomNode = {
+  childCount: 1,
+  className: 'MediaPreviewImageWrapper',
+  type: VirtualDomElements.Div,
+}
+
 const renderError = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: 'MediaPreviewError',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 1,
-      type: VirtualDomElements.Span,
-    },
-    text('Image could not be loaded'),
-  ]
+  return [errorNode, errorMessageNode, text('Image could not be loaded')]
 }
 
 const renderImage = (state: Readonly<MediaPreviewState>): readonly VirtualDomNode[] => {
   const { url } = state
   return [
-    {
-      childCount: 1,
-      className: 'MediaPreviewContent',
-      type: VirtualDomElements.Div,
-    },
-    {
-      childCount: 1,
-      className: 'MediaPreviewImageWrapper',
-      type: VirtualDomElements.Div,
-    },
+    contentNode,
+    imageWrapperNode,
     {
       alt: '',
       childCount: 0,
       className: 'MediaPreviewImage',
       draggable: false,
       name: 'image',
-      onError: 'handleMediaPreviewImageError',
+      onError: handleMediaPreviewImageError,
       src: url,
       type: VirtualDomElements.Img,
     },
@@ -50,8 +58,8 @@ export const render = (state: Readonly<MediaPreviewState>): readonly VirtualDomN
     {
       childCount: 1,
       className,
-      onPointerDown: 'handleMediaPreviewPointerDown',
-      onWheel: 'handleMediaPreviewWheel',
+      onPointerDown: handleMediaPreviewPointerDown,
+      onWheel: handleMediaPreviewWheel,
       type: VirtualDomElements.Div,
     },
     ...content,
