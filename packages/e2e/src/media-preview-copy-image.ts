5a04eff0275834cf27045ee91ee46f323f2dfb31
Le Vivilet <levivilet@pm.me>
2026-08-06 00:58:58 +0000

    test: verify copied image contents

--- packages/e2e/src/media-preview-copy-image.ts
+++ packages/e2e/src/media-preview-copy-image.ts
@@ -2,17 +2,24 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'media-preview-copy-image'
 
-export const test: Test = async ({ Command, expect, Locator, Main }) => {
+export const test: Test = async ({ ClipBoard, Command, expect, Locator, Main }) => {
   const imageUri = import.meta.resolve('../files/file.png')
   await Main.openUri(imageUri)
 
-  const image = Locator('.MediaPreviewImage')
-  await expect(image).toHaveCount(1)
-  const states = await Command.execute('Viewlet.getAllStates')
-  const mediaPreview = Object.values(states).find(({ viewId }: any) => viewId === 'builtin.media-preview') as any
-  await Command.execute('Viewlet.executeViewletCommand', mediaPreview.uid, 'handleContextMenu', 'image', 10, 10)
-  const copyImage = Locator('.MenuItem').nth(1)
-  await expect(copyImage).toHaveText('Copy Image')
-  // eslint-disable-next-line e2e/no-direct-click -- Image clipboard writes require a trusted user gesture.
-  await copyImage.click()
+  await ClipBoard.enableMemoryClipBoard()
+  try {
+    const image = Locator('.MediaPreviewImage')
+    await expect(image).toHaveCount(1)
+    const states = await Command.execute('Viewlet.getAllStates')
+    const mediaPreview = Object.values(states).find(({ viewId }: any) => viewId === 'builtin.media-preview') as any
+    await Command.execute('Viewlet.executeViewletCommand', mediaPreview.uid, 'handleContextMenu', 'image', 10, 10)
+    const copyImage = Locator('.MenuItem').nth(1)
+    await expect(copyImage).toHaveText('Copy Image')
+    // eslint-disable-next-line e2e/no-direct-click -- Image clipboard writes require a trusted user gesture.
+    await copyImage.click()
+
+    await ClipBoard.shouldHaveImage(imageUri)
+  } finally {
+    await ClipBoard.disableMemoryClipBoard()
+  }
 }
