16e78e3f69bf5e040066053ffbb2ee7690b52959
Le Vivilet <levivilet@pm.me>
2026-08-09 21:57:31 +0000

    fix: align image conversion worker types

--- packages/image-conversion-worker/src/imageConversionWorkerMain.ts
+++ packages/image-conversion-worker/src/imageConversionWorkerMain.ts
@@ -1,11 +1,12 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { WebWorkerRpcClient, type Rpc } from '@lvce-editor/rpc'
 import * as CommandMap from './parts/CommandMap/CommandMap.ts'
 
 export { commandMap } from './parts/CommandMap/CommandMap.ts'
 
-if (!globalThis.rpc) {
-  // eslint-disable-next-line unicorn/no-global-object-property-assignment
-  globalThis.rpc = await WebWorkerRpcClient.create({
+const globalScope = globalThis as typeof globalThis & { rpc?: Rpc }
+
+if (!globalScope.rpc) {
+  globalScope.rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
 }
