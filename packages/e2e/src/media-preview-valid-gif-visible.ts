9d0e1268ccdc80631f230fe4d877de251f589fd0
Le Vivilet <levivilet@pm.me>
2026-08-09 22:00:02 +0000

    test: isolate gif preview coverage

--- packages/e2e/src/media-preview-valid-gif-visible.ts
+++ packages/e2e/src/media-preview-valid-gif-visible.ts
@@ -1,6 +1,6 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'media-preview-gif-visible'
+export const name = 'media-preview-valid-gif-visible'
 
 export const test: Test = async ({ expect, Locator, Main }) => {
   await Main.openUri(import.meta.resolve('../files/sample.gif'))
