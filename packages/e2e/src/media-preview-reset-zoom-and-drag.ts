8430234fbcb331589ba6e38f09a6e642f8f9c231
Le Vivilet <levivilet@pm.me>
2026-08-11 20:22:29 +0000

    test: isolate reset image scenario

--- packages/e2e/src/media-preview-reset-zoom-and-drag.ts
+++ packages/e2e/src/media-preview-reset-zoom-and-drag.ts
@@ -1,14 +1,10 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'media-preview-context-menu'
+export const name = 'media-preview-reset-zoom-and-drag'
 
 export const test: Test = async ({ Command, ContextMenu, expect, Locator, Main }) => {
   const imageUri = import.meta.resolve('../files/file.png')
   await Main.openUri(imageUri)
-
-  const image = Locator('.MediaPreviewImage')
-  await expect(image).toHaveCount(1)
-
   const states = await Command.execute('Viewlet.getAllStates')
   const mediaPreview = Object.values(states).find(({ viewId }: any) => viewId === 'builtin.media-preview') as any
   await Command.execute(
@@ -45,19 +41,8 @@ export const test: Test = async ({ Command, ContextMenu, expect, Locator, Main }
     20,
   )
   const content = Locator('.MediaPreviewContent')
-  await expect(content).not.toHaveCSS('transform', 'matrix(1, 0, 0, 1, 0, 0)')
+  await expect(content).toHaveCSS('transform', 'matrix(1.5, 0, 0, 1.5, 10, 20)')
   await Command.execute('Viewlet.executeViewletCommand', mediaPreview.uid, 'handleContextMenu', 'image', 10, 10)
-
-  const menuItems = Locator('.MenuItem')
-  const copyPath = menuItems.nth(0)
-  const copyImage = menuItems.nth(1)
-  const resetImage = menuItems.nth(2)
-  await expect(menuItems).toHaveCount(3)
-  await expect(copyPath).toHaveText('Copy Path')
-  await expect(copyImage).toHaveText('Copy Image')
-  await expect(resetImage).toHaveText('Reset Image')
-
   await ContextMenu.selectItem('Reset Image')
-
   await expect(content).toHaveCSS('transform', 'matrix(1, 0, 0, 1, 0, 0)')
 }
