42b25ba959b1bff0dcc926adf2ab8b7f28aac3f2
Le Vivilet <levivilet@pm.me>
2026-08-24 20:16:08 +0000

    fix: use portable disk path in media preview test

--- packages/e2e/src/media-preview-missing-absolute-path-error.ts
+++ packages/e2e/src/media-preview-missing-absolute-path-error.ts
@@ -2,8 +2,13 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'media-preview-missing-absolute-path-error'
 
-export const test: Test = async ({ expect, Locator, Main }) => {
-  await Main.openUri('/__lvce_media_preview_missing__.svg')
+const toAbsolutePath = (fileUri: string): string => {
+  return /^file:\/\/\/[a-z]:/i.test(fileUri) ? fileUri.slice('file:///'.length) : fileUri.slice('file://'.length)
+}
+
+export const test: Test = async ({ expect, FileSystem, Locator, Main }) => {
+  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
+  await Main.openUri(`${toAbsolutePath(tmpDir)}/missing.svg`)
 
   const error = Locator('.MediaPreviewError')
   const openInTextEditor = Locator('.MediaPreviewOpenInTextEditor')
